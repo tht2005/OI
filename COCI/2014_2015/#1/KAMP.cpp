94e777c323839fbe0f49b906e4aa005c3258d66f
tht2005 <dangduong31205@gmail.com>
2021-08-26 13:04:35 +0700

    coci kamp

---
+++ COCI/2014_2015/#1/KAMP.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b);i<=_b;++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b);i>=_b;--i)
+#define FORE(i,a) for(auto&i:a)
+#define rep(i,b) for(int i=0,_b=(b);i<_b;++i)
+#define SZ(a) ((int)a.size())
+#define ALL(a) a.begin(),a.end()
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    return 0;
+}
