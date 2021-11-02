c21228c2d5c1456c41131f40ac0227207bbfcec2
tht2005 <75089048+tht2005@users.noreply.github.com>
2021-11-03 01:19:37 +0700

    Create h.cpp

---
+++ cf gym/100168/h.cpp
@@ -0,0 +1,132 @@
+/**
+ *	Author: tht2005
+ *	Gene: 2021.11.02 15:10:09	
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef __GNU_PBDS
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#endif
+
+#define fs first
+#define sc second
+#define SZ(x) ((int)x.size())
+#define ALL(x) x.begin(), x.end()
+
+using ll = long long;
+using pi = std::pair<int, int>;
+template<typename T> using vt = std::vector<T>;
+
+const double PI = acos(-1);
+
+struct pt
+{
+	double x, y;
+
+	pt() {}
+	pt(double x_, double y_) : x(x_), y(y_) {}
+	
+	bool operator== (pt rhs)
+	{
+		return x == rhs.x && y == rhs.y;
+	}
+
+	bool operator!= (pt rhs)
+	{
+		return x != rhs.x || y != rhs.y;
+	}
+
+	pt operator+ (pt rhs)
+	{
+		return pt(x + rhs.x, y + rhs.y);
+	}
+
+	pt operator- (pt rhs)
+	{
+		return pt(x - rhs.x, y - rhs.y);
+	}
+
+	pt operator* (double t)
+	{
+		return pt(x * t, y * t);
+	}
+
+	pt operator/ (double t)
+	{
+		return pt(x / t, y / t);
+	}
+};
+
+pt operator* (double a, pt b)
+{
+	return b * a;
+}
+
+double dot(pt a, pt b)
+{
+	return a.x * b.x + a.y * b.y;
+}
+
+double cross(pt a, pt b)
+{
+	return a.x * b.y - a.y * b.x;
+}
+
+double norm(pt a)
+{
+	return dot(a, a);
+}
+
+double abs(pt a)
+{
+	return sqrt(norm(a));
+}
+
+struct line
+{
+	double a, b, c;
+
+	line() {}
+	line(double a_, double b_, double c_) : a(a_), b(b_), c(c_) {}
+	line(pt p, pt q)
+	{
+		a = q.y - p.y;
+		b = p.x - q.x;
+		c = -a * p.x - b * p.y;
+	}
+};
+
+pt solveSLE(pt a, pt b, pt c)
+{
+	double d = cross(a, b);
+	return pt(cross(c, b) / d, cross(a, c) / d);
+}
+
+pt isect(line x, line y)
+{
+	return solveSLE(pt(x.a, y.a), pt(x.b, y.b), pt(-x.c, -y.c));
+}
+
+int main()
+{
+#ifndef LOCAL
+	freopen("distance3.in", "r", stdin);
+	freopen("distance3.out", "w", stdout);
+#endif
+	pt o, a, b;
+	scanf("%lf %lf %lf %lf %lf %lf", &o.x, &o.y, &a.x, &a.y, &b.x, &b.y);
+	double res = abs(cross(a - o, b - o) / abs(b - a));
+	if(dot(o - a, b - a) < 0) {
+		res = abs(o - a);
+	}
+	printf("%.10lf", res);
+#ifdef LOCAL
+	std::cerr << std::fixed << std::setprecision(5);
+	std::cerr << std::endl << "Time elapsed: " << ((float)clock() / CLOCKS_PER_SEC) << "s." << std::endl;
+#endif
+	return 0;
+}
