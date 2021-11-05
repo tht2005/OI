3666c053f5d22973d3616187c9ed66deda13ed73
tht2005 <75089048+tht2005@users.noreply.github.com>
2021-11-05 23:02:55 +0700

    Create n.cpp

---
+++ cf gym/100168/n.cpp
@@ -0,0 +1,110 @@
+/**
+ *	Author: tht2005
+ *	Gene: 2021.11.02 15:10:09	
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef __GNU_PBDS
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#endif
+
+#define fs first
+#define sc second
+#define SZ(x) ((int)x.size())
+#define ALL(x) x.begin(), x.end()
+
+using ll = long long;
+using pi = std::pair<int, int>;
+template<typename T> using vt = std::vector<T>;
+
+const double PI = acos(-1);
+
+struct pt
+{
+	double x, y;
+
+	pt() {}
+	pt(double x_, double y_) : x(x_), y(y_) {}
+	
+	bool operator== (pt rhs)
+	{
+		return x == rhs.x && y == rhs.y;
+	}
+
+	bool operator!= (pt rhs)
+	{
+		return x != rhs.x || y != rhs.y;
+	}
+
+	pt operator+ (pt rhs)
+	{
+		return pt(x + rhs.x, y + rhs.y);
+	}
+
+	pt operator- (pt rhs)
+	{
+		return pt(x - rhs.x, y - rhs.y);
+	}
+
+	pt operator* (double t)
+	{
+		return pt(x * t, y * t);
+	}
+
+	pt operator/ (double t)
+	{
+		return pt(x / t, y / t);
+	}
+};
+
+pt operator* (double a, pt b)
+{
+	return b * a;
+}
+
+double dot(pt a, pt b)
+{
+	return a.x * b.x + a.y * b.y;
+}
+
+double cross(pt a, pt b)
+{
+	return a.x * b.y - a.y * b.x;
+}
+
+double norm(pt a)
+{
+	return dot(a, a);
+}
+
+double abs(pt a)
+{
+	return sqrt(norm(a));
+}
+
+pt solveSLE(pt a, pt b, pt c)
+{
+	double d = cross(a, b);
+	return pt(cross(c, b) / d, cross(a, c) / d);
+}
+
+int main()
+{
+#ifndef LOCAL
+	freopen("line2.in", "r", stdin);
+	freopen("line2.out", "w", stdout);
+#endif
+	ll xA, yA, dx, dy;
+	scanf("%lld %lld %lld %lld", &xA, &yA, &dy, &dx);
+	ll xB = xA - dx, yB = yA + dy;
+	printf("%lld %lld %lld", yB - yA, xA - xB, -xA * (yB - yA) - yA * (xA - xB));
+#ifdef LOCAL
+	std::cerr << std::fixed << std::setprecision(5);
+	std::cerr << std::endl << "Time elapsed: " << ((float)clock() / CLOCKS_PER_SEC) << "s." << std::endl;
+#endif
+	return 0;
+}
